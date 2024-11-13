e8cc1bb26f0703bbe3ef27c963089bc51b19b67b
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2024-11-12 22:59:38 -0500

    Added flag to only trigger pop up once each iteration

--- scripts/content.js
+++ scripts/content.js
@@ -1,3 +1,7 @@
+//global flag to ensure popup doesnt trigger more than once each iteration
+let popupTriggered = false;
+
+//function that handles the highlighted word alert.
 function foo() {
   const selObj = window.getSelection().toString().trim();
   if (selObj != "") {
@@ -5,8 +9,15 @@ function foo() {
   }
 }
 
+//simply call the function after a certain time
 document.addEventListener("mouseup", () => {
-  setTimeout(() => {
-    foo();
-  }, 1000);
+  if (!popupTriggered) {
+    popupTriggered = true;
+    setTimeout(() => {
+      foo();
+      setTimeout(() => {
+        popupTriggered = false;
+      }, 100);
+    }, 500);
+  }
 });
