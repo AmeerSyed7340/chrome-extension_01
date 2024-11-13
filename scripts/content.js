bfc35344bd51712589d703ba4fb2316fabfaa6bc
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2024-11-13 04:46:11 -0500

    Added a separate div to the side to display highlighted sentences

--- scripts/content.js
+++ scripts/content.js
@@ -5,7 +5,35 @@ let popupTriggered = false;
 function foo() {
   const selObj = window.getSelection().toString().trim();
   if (selObj != "") {
-    alert(selObj);
+    createPopUp(selObj);
+  }
+}
+
+//function to create the separate pop up
+function createPopUp(textInput) {
+  //check if popup already exists
+  let existingPopup = document.getElementById("popup-div");
+
+  if (!existingPopup) {
+    //create a new div element
+    const newDiv = document.createElement("div");
+
+    //give it content
+    const divContent = document.createTextNode(textInput);
+
+    //add the text node to the new div
+    newDiv.appendChild(divContent);
+
+    //apply the css class to the div and the ID
+    newDiv.className = "popup-style";
+    newDiv.id = "popup-div";
+
+    //Append to the html body
+    document.body.appendChild(newDiv);
+  }
+  else{
+    //If the popup already exists then update its content
+    existingPopup.textContent = textInput;
   }
 }
 
