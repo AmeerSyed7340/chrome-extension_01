f013d6551166e52936f1488a602f5828ca809b1e
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2024-11-13 05:15:01 -0500

    Added eventlistener to handle click outside popup

--- scripts/content.js
+++ scripts/content.js
@@ -3,8 +3,10 @@ let popupTriggered = false;
 
 //function that handles the highlighted word alert.
 function foo() {
+  
   const selObj = window.getSelection().toString().trim();
   if (selObj != "") {
+    //create the new popup
     createPopUp(selObj);
   }
 }
@@ -34,6 +36,29 @@ function createPopUp(textInput) {
   else{
     //If the popup already exists then update its content
     existingPopup.textContent = textInput;
+  }
+
+  //add an event listener to remove the popup if clicked outside
+  document.addEventListener('click', handleClickOutsidePopup);
+}
+
+//remove popup div
+function removePopup(){
+  const existingPopup = document.getElementById("popup-div");
+  if(existingPopup){
+    existingPopup.remove(); //removes itself
+  }
+}
+
+//event handler for clicking outside the popup
+function handleClickOutsidePopup(event){
+  const popup = document.getElementById("popup-div");
+  if(popup && !popup.contains(event.target)){
+    // Click happened outside the popup, so remove it
+    popup.remove();
+
+    // Remove this event listener to avoid unnecessary calls
+    document.removeEventListener("click", handleClickOutsidePopup);
   }
 }
 
