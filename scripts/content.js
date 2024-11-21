dbb813239afbe42fdd1a455d45e3a763f91bd71e
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2024-11-21 09:02:24 -0500

    Set up service worker successfully that sends response back to content.js

--- scripts/content.js
+++ scripts/content.js
@@ -4,70 +4,18 @@ let popupTriggered = false;
 //function that handles the highlighted word alert.
 function foo() {
   const selObj = window.getSelection().toString().trim();
-  if (selObj != "") {
-    console.log(selObj);
+  if (selObj) {
+    console.log("Highlighted text:", selObj);
+
+    //message passing api - single message
+     (async () => {
+      const response = await chrome.runtime.sendMessage({greeting: "hello"});
+      // do something with response here, not outside the function
+      console.log(response);
+     })();
     
   }
 }
-
-// //function to create the separate pop up
-// function createPopUp(textInput) {
-
-//   //check if popup already exists
-//   let existingPopup = document.getElementById("popup-div");
-
-//   if (!existingPopup) {
-//     //create a new div element
-//     const newDiv = document.createElement("div");
-
-//     //create a button for testing atm
-//     const newBtn = document.createElement("button");
-
-//     //apply the css class to the div and the ID
-//     newDiv.className = "popup-style";
-//     newDiv.id = "popup-div";
-
-//     //set text content of new div to the input param
-//     newDiv.textContent = textInput;
-
-//     //apply css class to button
-//     newBtn.className = "btn-style";
-//     //set the name of the button
-//     newBtn.textContent = "Summarize";
-//     //what happens when the button is clicked
-//     newBtn.addEventListener("click", (event)=>{
-//       event.preventDefault();
-
-//       // //stop the button click to re-trigger the foo via mouseup event
-//       // event.stopPropagation();
-//       console.log("Btn Clicked");
-//     })
-
-//     //append button to newDiv
-//     newDiv.appendChild(newBtn);
-
-//     //Append to the html body
-//     document.body.appendChild(newDiv);
-
-//     //add an event listener to remove the popup if clicked outside
-//     document.addEventListener("click", handleClickOutsidePopup);
-//   } else {
-//     //If the popup already exists then update its content
-//     existingPopup.textContent = textInput;
-//   }
-// }
-
-// //event handler for clicking outside the popup
-// function handleClickOutsidePopup(event) {
-//   const popup = document.getElementById("popup-div");
-//   if (popup && !popup.contains(event.target)) {
-//     // Click happened outside the popup, so remove it
-//     popup.remove();
-
-//     // Remove this event listener to avoid unnecessary calls
-//     document.removeEventListener("click", handleClickOutsidePopup);
-//   }
-// }
 
 //simply call the function after a certain time
 document.addEventListener("mouseup", () => {
