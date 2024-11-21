a5ea8f2bafe712d67c879aff07dcbe3dd88c5fc2
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2024-11-21 06:31:05 -0500

    Added a popup.js file to seprate responsibility of popup functionality and highlighting words

--- scripts/content.js
+++ scripts/content.js
@@ -1,66 +1,73 @@
-//global flag to ensure popup doesnt trigger more than once each iteration
+//Popup trigger to remove duplicate clicks
 let popupTriggered = false;
 
 //function that handles the highlighted word alert.
 function foo() {
-  
   const selObj = window.getSelection().toString().trim();
   if (selObj != "") {
-    //create the new popup
-    createPopUp(selObj);
+    console.log(selObj);
+    
   }
 }
 
-//function to create the separate pop up
-function createPopUp(textInput) {
-  //check if popup already exists
-  let existingPopup = document.getElementById("popup-div");
+// //function to create the separate pop up
+// function createPopUp(textInput) {
 
-  if (!existingPopup) {
-    //create a new div element
-    const newDiv = document.createElement("div");
+//   //check if popup already exists
+//   let existingPopup = document.getElementById("popup-div");
 
-    //give it content
-    const divContent = document.createTextNode(textInput);
+//   if (!existingPopup) {
+//     //create a new div element
+//     const newDiv = document.createElement("div");
 
-    //add the text node to the new div
-    newDiv.appendChild(divContent);
+//     //create a button for testing atm
+//     const newBtn = document.createElement("button");
 
-    //apply the css class to the div and the ID
-    newDiv.className = "popup-style";
-    newDiv.id = "popup-div";
+//     //apply the css class to the div and the ID
+//     newDiv.className = "popup-style";
+//     newDiv.id = "popup-div";
 
-    //Append to the html body
-    document.body.appendChild(newDiv);
-  }
-  else{
-    //If the popup already exists then update its content
-    existingPopup.textContent = textInput;
-  }
+//     //set text content of new div to the input param
+//     newDiv.textContent = textInput;
 
-  //add an event listener to remove the popup if clicked outside
-  document.addEventListener('click', handleClickOutsidePopup);
-}
+//     //apply css class to button
+//     newBtn.className = "btn-style";
+//     //set the name of the button
+//     newBtn.textContent = "Summarize";
+//     //what happens when the button is clicked
+//     newBtn.addEventListener("click", (event)=>{
+//       event.preventDefault();
 
-//remove popup div
-function removePopup(){
-  const existingPopup = document.getElementById("popup-div");
-  if(existingPopup){
-    existingPopup.remove(); //removes itself
-  }
-}
+//       // //stop the button click to re-trigger the foo via mouseup event
+//       // event.stopPropagation();
+//       console.log("Btn Clicked");
+//     })
 
-//event handler for clicking outside the popup
-function handleClickOutsidePopup(event){
-  const popup = document.getElementById("popup-div");
-  if(popup && !popup.contains(event.target)){
-    // Click happened outside the popup, so remove it
-    popup.remove();
+//     //append button to newDiv
+//     newDiv.appendChild(newBtn);
 
-    // Remove this event listener to avoid unnecessary calls
-    document.removeEventListener("click", handleClickOutsidePopup);
-  }
-}
+//     //Append to the html body
+//     document.body.appendChild(newDiv);
+
+//     //add an event listener to remove the popup if clicked outside
+//     document.addEventListener("click", handleClickOutsidePopup);
+//   } else {
+//     //If the popup already exists then update its content
+//     existingPopup.textContent = textInput;
+//   }
+// }
+
+// //event handler for clicking outside the popup
+// function handleClickOutsidePopup(event) {
+//   const popup = document.getElementById("popup-div");
+//   if (popup && !popup.contains(event.target)) {
+//     // Click happened outside the popup, so remove it
+//     popup.remove();
+
+//     // Remove this event listener to avoid unnecessary calls
+//     document.removeEventListener("click", handleClickOutsidePopup);
+//   }
+// }
 
 //simply call the function after a certain time
 document.addEventListener("mouseup", () => {
